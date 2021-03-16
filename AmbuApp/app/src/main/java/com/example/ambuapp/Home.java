af75af5257e735cecd920781d08a6b51a78164ce
Thien <thien_@hotmail.fi>
2021-03-16 06:51:14 +0200

    app

---
+++ AmbuApp/app/src/main/java/com/example/ambuapp/Home.java
@@ -0,0 +1,22 @@
+package com.example.ambuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+    public void openActivity(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        setResult(RESULT_OK, intent);
+        startActivity(intent);
+    }
+}
